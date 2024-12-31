2911ba38cb9bf7aa91527a03f91369971fff4da1
RedBlogShark <RedStoneShark@163.com>
2024-12-31 23:42:55 +0800

    Site updated: 2024-12-31 23:42:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/21/hello-world/","2024/12/21/I-m-RedStoneShark/"];function toRandomPost(){
+var posts=["2024/12/21/I-m-RedStoneShark/","2024/12/21/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
